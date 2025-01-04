4de3ad8f3a59d8b8dfa46153b4981006fc1d1b0c
imaerials <arielro85@gmail.com>
2025-01-04 20:56:15 +0000

    adds rt and rtassoc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,13 +2,14 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "${var.vpc_name}-igw"
   }
 }
+
 
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
@@ -20,25 +21,52 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr_blocks[count.index]
   count                  = var.subnet_count.public
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index + 1]  // Use a different AZ
   
   tags = {
     "Name" = "${var.vpc_name}-public-${count.index}"
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private_subnet" {
   vpc_id      = aws_vpc.main.id
   cidr_block  = var.private_subnet_cidr_blocks[count.index] 
   count                  = var.subnet_count.private
-availability_zone = data.aws_availability_zones.available.names[count.index]
+availability_zone = "us-east-1a"
   
   tags = {
     "Name" = "${var.vpc_name}-private-${count.index}"
   }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+}
+resource "aws_route_table_association" "public" {
+  count          = var.subnet_count.public
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.subnet_count.private
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rt.id
 }
 
